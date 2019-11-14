14695adddf49952047fcc0bf9c0ff90736c8f898
Anton Webb <anton25360@gmail.com>
2019-11-14 13:30:37 +0000

    Created 'completed tasks' page

--- src/routes.php
+++ src/routes.php
@@ -7,12 +7,22 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
+    //home page (displays tasks)
     $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
 
         // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
+        return $container->get('renderer')->render($response, 'homepage.phtml', $args);
+    });
+
+    //completed page (displays done tasks)
+    $app->get('/completed', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/' route");
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'completed.phtml', $args);
     });
 
 };
